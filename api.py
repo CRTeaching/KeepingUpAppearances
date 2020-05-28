6c39161f8ff8817314f912ca5838f558acefd110
fategas <normalplek@gmail.com>
2020-05-28 12:45:11 +0200

    Add ability to see Train/ and Test/ images via API

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 ### THIS WILL BE THE UNDERLYING API.
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_from_directory
+from flask import flash, request, redirect
 import os
 
 app = Flask(__name__)
@@ -19,6 +20,29 @@ def show_images():
     #print(images)
     return render_template("show_images.html", images=images)
 
+@app.route('/Test/')
+def show_testing_images():
+    images = []
+    # Load in all the images.
+    folder = os.listdir('static/Test/')
+    for image in folder:
+        images.append(image)
+    return render_template("show_testing_images.html", images=images)
+
+@app.route('/Train/')
+def show_training_images():
+    images = []
+        # Load in all the images.
+    folder = os.listdir('static/Train/')
+    number_of_files = 0
+    for image in folder:
+        number_of_files += 1    
+        images.append(image)
+        if number_of_files%50 == 0:
+            print(number_of_files)
+        if number_of_files == 800: #if number of images equals the batch_size of 800, stop.
+            break
+    return render_template("show_training_images.html", images=images)
 @app.route('/colorize/')
 def colorize_images(folder = "Test/", model_name='new_model'):
     from utils import save_the_images, loadModel, prepare_accuracy_visualisation_images
@@ -35,9 +59,32 @@ def colorize_images(folder = "Test/", model_name='new_model'):
     # Finally, Save the colorized images
     save_the_images(output, color_me)
     return "It worked!"
-#@app.route('/img/<path:filename>') 
-#def send_file(filename): 
-    #return send_from_directory(filename)
 
+@app.route('/imagine/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        filename = file.filename
+        file.save(os.path.join('images', filename)) #check back on 'images'
+        return redirect(url_for('uploaded_file',filename=filename))
+    return '''
+        <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 if __name__ == "__main__":
     app.run(debug=True)
